49871640184f8f5f4a61e400347a5cfedce06b00
吳元皓 <hpware3@gmail.com>
2024-12-10 23:49:16 +0800

    Fix getToken function to remove commented code and return the access token

--- server/components/transport/getToken.ts
+++ server/components/transport/getToken.ts
@@ -32,20 +32,12 @@ async function getToken() {
         },
     body: `client_id=${clientId}&client_secret=${clientSecret}&grant_type=${grantType}`,
     });
-<<<<<<< HEAD
     const data = await response.json(); 
-=======
-    //const data = await response.json(); 
->>>>>>> 0c62103618c64b0e9dc0bf2ee7c0db19578bdf05
     //console.log('Using newly created token\n' + data.access_token);
     tokenCache.set(reqtrackingkey, data.access_token);
     return data.access_token;
     // The code above must be uncommented in prod
-<<<<<<< HEAD
     //return null;
-=======
-    return 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJER2lKNFE5bFg4WldFajlNNEE2amFVNm9JOGJVQ3RYWGV6OFdZVzh3ZkhrIn0.eyJleHAiOjE3MzM3NTU2NjgsImlhdCI6MTczMzY2OTI2OCwianRpIjoiYWYwMmExZjItZDk2YS00OTE4LWFkODktNjEyMGIyNjQ5ZjEyIiwiaXNzIjoiaHR0cHM6Ly90ZHgudHJhbnNwb3J0ZGF0YS50dy9hdXRoL3JlYWxtcy9URFhDb25uZWN0Iiwic3ViIjoiZDc5ZDk5OTYtMDE3Mi00NTAzLWFlZjAtZTAzNzYyMTdiMzRlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiczUxMzMxMTAyLThkYzM0NDllLTE5ODMtNDY0MSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsic3RhdGlzdGljIiwicHJlbWl1bSIsInBhcmtpbmdGZWUiLCJtYWFzIiwiYWR2YW5jZWQiLCJnZW9pbmZvIiwidmFsaWRhdG9yIiwidG91cmlzbSIsImhpc3RvcmljYWwiLCJjd2EiLCJiYXNpYyJdfSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwidXNlciI6IjhiYWUyMTM2In0.GLBKj_JRTGdXJTrcpWrGnxpYL7v7J18eK1Wc30bYfD99kV6f0hmJoVSfqgd9qPQaLxSdQiq6RCCSNr627G9uSrgITzRuyk4yxrkV_kFtcMouitjPzvnukdIxNIhVCu-kPU3W9Oa6REaqbWmu0zSGxW92rWJeldyICG-c25D2PkTiIl0uNCvaf_xayyrl5g4_ujIBZQSw2VbKBEp4_IzwFDO79s8p8PS7NKBcMQhN_S-WjUOUcuJIyd_87nz59sogsyLOi8SXm6azi7s30PG5Q4wLdIeeZ3sjrOSTHk8efg6O_qyeyGEXsWJuffhBTdCk99svwj0NimObFTpFINXTew';
->>>>>>> 0c62103618c64b0e9dc0bf2ee7c0db19578bdf05
     
 }
 export default getToken;
