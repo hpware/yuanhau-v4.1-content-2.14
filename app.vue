37f98ee582ce478fdba4a327b0731f54a91aad2e
吳元皓 <hpware3@gmail.com>
2024-12-22 01:08:35 +0800

    Refactor footer styles in app.vue and error.vue components

--- app.vue
+++ app.vue
@@ -12,17 +12,18 @@ import { defineProps, ref, onMounted } from "vue";
 </template>
 <style scoped>
 footer {
-  position:fixed;
-  bottom:1rem;
-  text-align: center;
-  color:rgba(255, 255, 255, 0.829);
-  p {
-    font-size: 0.9em;
-    margin-bottom: 0;
-  }
-  a {
-    text-decoration: none;
-    font-size: 0.8em;
-  }
+      position:fixed;
+      bottom:1rem;
+      text-align: center;
+      color:rgba(255, 255, 255, 0.829);
+      p {
+        font-size: 0.6em;
+        margin-bottom: 0;
+        margin-left: 0.5em;
+      }
+       a {
+         text-decoration: none;
+         font-size: 0.8em;
+       }
 }
 </style>
--- error.vue
+++ error.vue
@@ -1,15 +1,37 @@
 <script setup lang="ts">
 import NavBar from '~/components/NavBar.vue';
 const error = useError();
+useHead({
+    title: `系統錯誤 | 吳元皓`,
+})
 </script>
 
 <template>
     <NuxtLayout>
-        
-        <div>
+        <NavBar/>
+        <main>
             <h1>{{error.statusCode}}</h1>
-            <p> 穿梭者，你好<br/>你已經踏入到一個所人未知的地方<br />
-            </p>
-        </div>
+            <h3>{{error.message}}</h3>
+        </main>
+        <footer>
+            <p>Copyright © {{ new Date().getFullYear() }} 吳元皓</p>
+        </footer>
     </NuxtLayout>
 </template>
+<style scoped>
+footer {
+      position:fixed;
+      bottom:1rem;
+      text-align: center;
+      color:rgba(255, 255, 255, 0.829);
+      p {
+        font-size: 0.6em;
+        margin-bottom: 0;
+        margin-left: 0.5em;
+      }
+       a {
+         text-decoration: none;
+         font-size: 0.8em;
+       }
+}
+</style>
