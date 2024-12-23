37958bb3a804fe54a79944d13af2e475733730d9
flash_user <hw@yuanhau.com>
2024-12-23 11:01:13 +0800

    Add Giscus comment integration and update dependencies

--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import Giscus from '@giscus/vue'
+</script>
 <template>
     <main>
       <ContentDoc>
@@ -6,6 +9,21 @@
             <h1>{{ doc.title }}</h1>
             <ContentRenderer :value="doc" />
           </article>
+          <Giscus 
+            repo="hpware/posts"
+            repo-id="R_kgDONg6K8Q"
+        category="posts"
+           category-id="DIC_kwDONg6K8c4Clc9m"
+           mapping="title"
+           strict="0"
+            reactions-enabled="1"
+            emit-metadata="0"
+            input-position="bottom"
+            theme="preferred_color_scheme"
+            lang="zh-TW"
+            loading="lazy"
+            crossorigin="anonymous"
+            async/>
         </template>
         <template #not-found>
             <h1>沒有這個文章 >_<</h1>
