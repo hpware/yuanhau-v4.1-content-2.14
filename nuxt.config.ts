e57d698e074f6845bd10f26815cba65d22537f4c
吳元皓 <hpware3@gmail.com>
2025-01-01 14:11:10 +0800

    New Year, Neo SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,7 +66,7 @@ export default defineNuxtConfig({
         {
           name: "keywords",
           content:
-            "吳元皓,吳元皓的個人網站,吳元皓的個人網站首頁,吳元皓的個人網站首頁,Web Dev,Photography,Computer Nerd,Howard Wu,yuanhau,wuyuanhau,yuanhau.com,yuanh.xyz,Yuan-Hau Wu,吳元皓,元皓,吳元皓,吳元浩,元浩,吳元浩,吳元浩,五專生,ictechz,攝影,前端方面,台灣的五專生,使用Vue製作,吴元皓,元皓,吴元皓的网站,吴元浩,元浩,吴元浩的网站,吴元浩,五专生,ictechz,摄影,前端方面,台湾的五专生,使用Nuxt制作,Made With Nuxt,student,HTML Dev, Blog Posts, Photo Library, PHP Plugins, Profile Links, JS, Javascript, NextJS, HTML nerd, CSS Dev, CSS, HTML, Web Developer, HTML Developer, CSS Developer, Web Dev, 学生, HTML 开发, 博客文章, 照片库, PHP 插件, 个人资料链接, JS, Javascript, NextJS, CSS 开发, CSS, HTML, Web 开发人员, HTML 开发人员, CSS 开发人员, Web 开发, 學生, HTML 開發, 部落格文章, 照片庫, PHP 插件, 個人資料連結, JS, Javascript, NextJS, CSS 開發, CSS, HTML, Web 開發人員, HTML 開發人員, CSS 開發人員, Web 開發, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli, Familiar with HTML, Familiar with CSS, Familiar with Vercel, Familiar with Git, Familiar with Debian Cli, Familiar with Ubuntu Cli, Familiar with Debian Cli, Familiar with Ubuntu Cli, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli",
+            "吳元皓, 吳元皓的個人網站, 吳元皓的個人網站首頁, Howard Wu, yuanhau, wuyuanhau, yuanhau.com, yuanh.xyz, Yuan-Hau Wu, 吳元皓, 元皓, 吳元皓, 吳元浩, 元浩, 吳元浩, 吳元浩, 五專生, ictechz, 台灣的五專生, 吴元皓, 吴元皓的网站,吴元浩,元浩,吴元浩的网站,吴元浩,五专生,ictechz,摄影,前端方面, 個人資料連結",
         },
         { name: "author", content: "吳元皓" },
         { name: "author-email", content: "hw@yuanhau.com" },
@@ -87,12 +87,11 @@ export default defineNuxtConfig({
         {
           name: "og:keywords",
           content:
-            "吳元皓,吳元皓的個人網站,吳元皓的個人網站首頁,吳元皓的個人網站首頁,Web Dev,Photography,Computer Nerd,Howard Wu,yuanhau,wuyuanhau,yuanhau.com,yuanh.xyz,Yuan-Hau Wu,吳元皓,元皓,吳元皓,吳元浩,元浩,吳元浩,吳元浩,五專生,ictechz,攝影,前端方面,台灣的五專生,使用Vue製作,吴元皓,元皓,吴元皓的网站,吴元浩,元浩,吴元浩的网站,吴元浩,五专生,ictechz,摄影,前端方面,台湾的五专生,使用Nuxt制作,Made With Nuxt,student,HTML Dev, Blog Posts, Photo Library, PHP Plugins, Profile Links, JS, Javascript, NextJS, HTML nerd, CSS Dev, CSS, HTML, Web Developer, HTML Developer, CSS Developer, Web Dev, 学生, HTML 开发, 博客文章, 照片库, PHP 插件, 个人资料链接, JS, Javascript, NextJS, CSS 开发, CSS, HTML, Web 开发人员, HTML 开发人员, CSS 开发人员, Web 开发, 學生, HTML 開發, 部落格文章, 照片庫, PHP 插件, 個人資料連結, JS, Javascript, NextJS, CSS 開發, CSS, HTML, Web 開發人員, HTML 開發人員, CSS 開發人員, Web 開發, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli, Familiar with HTML, Familiar with CSS, Familiar with Vercel, Familiar with Git, Familiar with Debian Cli, Familiar with Ubuntu Cli, Familiar with Debian Cli, Familiar with Ubuntu Cli, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli",
+            "吳元皓, 吳元皓的個人網站, 吳元皓的個人網站首頁, Howard Wu, yuanhau, wuyuanhau, yuanhau.com, yuanh.xyz, Yuan-Hau Wu, 吳元皓, 元皓, 吳元皓, 吳元浩, 元浩, 吳元浩, 吳元浩, 五專生, ictechz, 台灣的五專生, 吴元皓, 吴元皓的网站,吴元浩,元浩,吴元浩的网站,吴元浩,五专生,ictechz,摄影,前端方面, 個人資料連結",
         },
         { name: "og:author", content: "吳元皓" },
         { name: "og:author:email", content: "hw@yuanhau.com" },
         { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-        { "http-equiv": "X-Frame-Options", content: "DENY" },
       ],
     },
   },
