0176dead9744ab561ffb6fa1ce105c2b1ed43e7c
yuanh <157942818+hpware@users.noreply.github.com>
2025-01-03 11:06:35 +0800

    Update recommendations.vue

--- pages/recommendations.vue
+++ pages/recommendations.vue
@@ -11,7 +11,7 @@ const func = async () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-      },
+      }
       //body: JSON.stringify({
         //discordhandle: discordhandle,
         //email: email,
