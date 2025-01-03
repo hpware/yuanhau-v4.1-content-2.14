dd7752215d9a90daaff883f01360c505512657de
yuanh <157942818+hpware@users.noreply.github.com>
2025-01-03 11:03:59 +0800

    Update recommendations.vue

--- pages/recommendations.vue
+++ pages/recommendations.vue
@@ -12,11 +12,11 @@ const func = async () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        discordhandle: discordhandle,
-        email: email,
-        message: message,
-    });
+      //body: JSON.stringify({
+        //discordhandle: discordhandle,
+        //email: email,
+        //message: message,
+    //});
   } catch (e) {
     console.error(e);
   } finally {
