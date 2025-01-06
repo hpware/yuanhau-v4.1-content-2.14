a731132d745efb1c9b62681d56d3accc983cbe3a
元皓yh <157942818+hpware@users.noreply.github.com>
2025-01-06 16:11:33 +0800

    Update projects.vue

--- pages/v3-pages/projects.vue
+++ pages/v3-pages/projects.vue
@@ -1,5 +1,9 @@
 <script setup>
 import Project from "@/components/pf-d/ProjectDisplayTemplate.vue";
+useHead({
+  title: "專案v3 | 吳元皓",
+  keywords: "Web Dev, Photography, Computer Nerd, Howard Wu, yuanhau, wuyuanhau, yuanhau.com, yuanh.xyz, Yuan-Hau Wu, 吳元皓, 元皓 ,吳元皓, 吳元浩, 元浩 ,吳元浩, 吳元浩 ,五專生, ictechz, 攝影, 前端方面, 台灣的五專生, 使用Vue製作, 吴元皓, 元皓 ,吴元皓的网站, 吴元浩, 元浩 ,吴元浩的网站, 吴元浩 ,五专生, ictechz, 摄影, 前端方面, 台湾的五专生, 使用Vue制作, Made With Vue, student, HTML Dev, Blog Posts, Photo Library, PHP Plugins, Profile Links, JS, Javascript, NextJS, HTML nerd, CSS Dev, CSS, HTML, Web Developer, HTML Developer, CSS Developer, Web Dev, 学生, HTML 开发, 博客文章, 照片库, PHP 插件, 个人资料链接, JS, Javascript, NextJS, CSS 开发, CSS, HTML, Web 开发人员, HTML 开发人员, CSS 开发人员, Web 开发, 學生, HTML 開發, 部落格文章, 照片庫, PHP 插件, 個人資料連結, JS, Javascript, NextJS, CSS 開發, CSS, HTML, Web 開發人員, HTML 開發人員, CSS 開發人員, Web 開發, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli, Familiar with HTML, Familiar with CSS, Familiar with Vercel, Familiar with Git, Familiar with Debian Cli, Familiar with Ubuntu Cli, Familiar with Debian Cli, Familiar with Ubuntu Cli, 熟悉使用 HTML, 熟悉使用CSS , 熟悉使用Vercel, 熟悉使用Git , 熟悉使用Debian Cli, 熟悉使用Ubuntu Cli, 熟悉使用 Debian Cli, 熟悉使用 Ubuntu Cli"
+})
 </script>
 <template>
   <h2 class="pagetitle">所有專案</h2>
