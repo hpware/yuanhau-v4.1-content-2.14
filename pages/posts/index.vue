0931f553a6e82f60cc767fdd4b4475ca646b8676
元皓yh <hw@yuanhau.com>
2025-04-02 09:49:29 +0800

    Update index.viewer

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -25,7 +25,7 @@ const query: QueryBuilderParams = {
       <h1 class="title">Blog</h1>
       <h6 class="dec">這裡是我的分享天地，逃離演算法的控制!(雖然有時候會壞)</h6>
     </div>
-    <div v-else class="list">
+    <div class="list">
       <!--<div class="search">
         <form @submit="">
           <input type="text" placeholder="Ex: Editor" />
